d83bd877851eb9e0913b172d6128d0b0d44a3966
PRIYANKA YADAV <123077030+yadmistigithub@users.noreply.github.com>
2024-04-04 09:40:54 +0530

    Add files via upload

---
+++ Alphabets.java
@@ -0,0 +1,11 @@
+public class Alphabets {
+    public static void main(String[] args) {
+        char i;
+        i='a';
+       while (i<'z') 
+       {
+        System.out.println(i);
+            i++;
+        }
+    }
+}
